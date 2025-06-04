81d0023e0cb335d41bd1a3e1da6262472ecd1293
Urapacito <urapacitoismyfakename@gmail.com>
2025-06-04 09:05:31 +0700

    ServiceFeaturesFunctions
    - View and Create functions added
    - Modify a bit for scalar route
    - Add new endpoints

---
+++ FoodVietAPI.Application/DTOs/ServiceFeatureDTO/CreateServiceFeatureDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanFoodVietAPI.Application.DTOs.ServiceFeatureDTO
+{
+    public record CreateServiceFeatureDTO
+    (
+         string ServiceFeatureName,
+         string? Description,
+         string DefaultValue
+    );
+}
+
