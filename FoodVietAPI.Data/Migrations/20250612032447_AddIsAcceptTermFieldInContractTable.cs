b179ab87ffd9bf9ae25cd3f295634320536596ba
hntduong12345 <hntduong2003@gmail.com>
2025-06-12 10:26:26 +0700

    refactor: update filed in subscription contract table
    Add IsAcceptTerm field for checking is the gardener accept term when purchasing Service Package

---
+++ FoodVietAPI.Data/Migrations/20250612032447_AddIsAcceptTermFieldInContractTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanFoodVietAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsAcceptTermFieldInContractTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAcceptTerm",
+                table: "SubscriptionContract",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAcceptTerm",
+                table: "SubscriptionContract");
+        }
+    }
+}
