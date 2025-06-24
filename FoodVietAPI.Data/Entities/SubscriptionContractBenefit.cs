81e3d65dce26358a90a9e3b0a1e90559c4eb1a88
Urapacito <urapacitoismyfakename@gmail.com>
2025-06-24 16:44:08 +0700

    SubscriptionContractBenefit
    - Now you wonder what this table does?
    + Store default value of features that contains in Gardener's bought package
    - Use to handle post quota count
    - Use for future upscale when Admin wants to change features without interrupt the Gardeners currently using it

---
+++ FoodVietAPI.Data/Entities/SubscriptionContractBenefit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanFoodVietAPI.Data.Entities
+{
+    public class SubscriptionContractBenefit
+    {
+        [Key]
+        public Ulid SubscriptionContractBenefitId { get; set; } = Ulid.NewUlid();
+
+        [Required]
+        public Ulid SubscriptionContractId { get; set; }
+
+        // Benefit type, e.g., "POST_QUOTA", "IMAGE_LIMIT", "POST_PRIORITY", etc.
+        [Required]
+        [MaxLength(50)]
+        public string BenefitType { get; set; }
+
+        // The default benefit value when the subscription is purchased.
+        [Required]
+        public int DefaultValue { get; set; }
+
+        // The current remaining value updated as the benefit is used.
+        [Required]
+        public int RemainingValue { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation property (using 'virtual' for consistency with other entities)
+        [ForeignKey(nameof(SubscriptionContractId))]
+        public virtual SubscriptionContract SubscriptionContract { get; set; }
+    }
+}
