d00b3a495ba818e0109cb981ff16498b1dfeb765
hntduong12345 <hntduong2003@gmail.com>
2025-06-29 10:31:16 +0700

    refactor: setup swagger ui

--- FoodVietAPI.Presentation/Extensions/DependencyServices.cs
+++ FoodVietAPI.Presentation/Extensions/DependencyServices.cs
@@ -10,6 +10,8 @@ using Stripe;
 using Stripe.Checkout;
 using CleanFoodVietAPI.Application.Utils;
 using System.Text;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 
 namespace CleanFoodVietAPI.Presentation.Extensions
@@ -69,6 +71,44 @@ namespace CleanFoodVietAPI.Presentation.Extensions
             services.AddScoped<IServicePackageOrderService, ServicePackageOrderService>();
             #endregion
 
+            return services;
+        }
+
+        public static IServiceCollection AddConfigSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo() { Title = "Clean Food APIs System", Version = "v1" });
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+            });
+                options.MapType<TimeOnly>(() => new OpenApiSchema
+                {
+                    Type = "string",
+                    Format = "time",
+                    Example = OpenApiAnyFactory.CreateFromJson("\"13:45:42.0000000\"")
+                });
+            });
             return services;
         }
 
--- FoodVietAPI.Presentation/Program.cs
+++ FoodVietAPI.Presentation/Program.cs
@@ -9,7 +9,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddStripeConfiguration(builder.Configuration);
 builder.Services.AddOpenApi("v1",
     options => { options.AddDocumentTransformer<BearerSecuritySchemeTransformer>(); });
-builder.Services.AddSwaggerGen();
 
 #region DI Setup
 builder.Services.AddCorsConfig();
@@ -17,6 +16,7 @@ builder.Services.AddDatabase(builder.Configuration);
 builder.Services.AddUnitOfWork();
 builder.Services.AddJwtAuthenticationScheme(builder.Configuration);
 builder.Services.AddServices(builder.Configuration);
+builder.Services.AddConfigSwagger();
 #endregion
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
