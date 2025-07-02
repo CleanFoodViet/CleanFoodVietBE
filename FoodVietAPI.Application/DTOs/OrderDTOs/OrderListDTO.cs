29a31c1146d493b819a2a7aee9bec2148aa9b060
hntduong12345 <hntduong2003@gmail.com>
2025-07-02 20:23:28 +0700

    feat: add order and order delivery apis controller

---
+++ FoodVietAPI.Application/DTOs/OrderDTOs/OrderListDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanFoodVietAPI.Application.DTOs.OrderDTOs
+{
+    public record OrderListDTO
+    (
+        Ulid OrderId,
+        Ulid RetailerId,
+        Ulid GardenerId,
+        string Status,
+        decimal TotalAmount,
+        DateTime CreatedAt,
+        int ProductTypeAmount
+    );
+}
---
+++ FoodVietAPI.Application/DTOs/OrderDeliveryDTOs/OrderDeliveryDetailDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanFoodVietAPI.Application.DTOs.OrderDeliveryDTOs
+{
+    public record OrderDeliveryDetailDTO
+    {
+        public Ulid OrderDeliveryDetailId { get; set; }
+        public Ulid ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public DateTime DeliveredAt { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string ProductUnit { get; set; } = null!;
+    }
+}
