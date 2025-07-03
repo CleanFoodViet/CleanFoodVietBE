4d189f0c73de46c8a2ac95543331b9b18c8c547c
Urapacito <urapacitoismyfakename@gmail.com>
2025-07-03 23:25:45 +0700

    Test azure log controller endpoint
    - Test azure log controller endpoint

--- FoodVietAPI.Presentation/Controllers/StripeWebhookController.cs
+++ FoodVietAPI.Presentation/Controllers/StripeWebhookController.cs
@@ -50,6 +50,13 @@ public class StripeWebhookController : ControllerBase
         }
     }
 
+    [HttpGet("/healthz")]
+    public IActionResult Healthz()
+    {
+        _logger.LogInformation("🔥 Healthz ping at {Now}", DateTime.UtcNow);
+        return Ok("alive");
+    }
+
     // Real webhook
     [HttpPost(ApiEndpointConstant.Webhook.StripeWebhookEndpoint)]
     public async Task<IActionResult> Post()
