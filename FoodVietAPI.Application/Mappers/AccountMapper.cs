016eca2c6a67cd60c3e3ebf1a91e814a13c725f1
Huynh Nguyen Thai Duong <115139619+hntduong12345@users.noreply.github.com>
2025-07-30 16:54:30 +0700

    fix: change the register flow of gardener
    Change Active status when 1st register to Pending

--- FoodVietAPI.Application/Mappers/AccountMapper.cs
+++ FoodVietAPI.Application/Mappers/AccountMapper.cs
@@ -28,7 +28,7 @@ namespace CleanFoodVietAPI.Application.Mappers
                 .ForMember(des => des.Password, opt => opt.MapFrom(src => HashUtil.PasswordHash(src.Password)))
                 .ForMember(des => des.Gender, opt => opt.MapFrom(src => src.Gender ?? AccountGenderEnum.MALE.ToString()))
                 .ForMember(des => des.Avatar, opt => opt.MapFrom(src => "None"))
-                .ForMember(des => des.Status, opt => opt.MapFrom(src => AccountStatusEnum.ACTIVE.ToString()))
+                .ForMember(des => des.Status, opt => opt.MapFrom(src => AccountStatusEnum.PENDING.ToString()))
                 .ForMember(des => des.IsVerified, opt => opt.MapFrom(src => false))
                 .ForMember(des => des.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
                 .ForMember(des => des.UpdatedAt, opt => opt.MapFrom(src => DateTime.UtcNow));
