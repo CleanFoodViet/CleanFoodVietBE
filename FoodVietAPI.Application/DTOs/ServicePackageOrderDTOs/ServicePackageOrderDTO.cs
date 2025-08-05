18feec4c4129fbc1b17f2268ab8f479f2b448e89
Huynh Nguyen Thai Duong <115139619+hntduong12345@users.noreply.github.com>
2025-08-05 18:19:49 +0700

    refactor: add service package name in get service package order apis

--- FoodVietAPI.Application/DTOs/ServicePackageOrderDTOs/ServicePackageOrderDTO.cs
+++ FoodVietAPI.Application/DTOs/ServicePackageOrderDTOs/ServicePackageOrderDTO.cs
@@ -13,6 +13,7 @@ namespace CleanFoodVietAPI.Application.DTOs.SubscriptionOrderDTOs
         public string GardenerPhone { get; set; } = null!;
 
         public Ulid ServicePackageId { get; set; }
+        public string? ServicePackageName { get; set; }
         public decimal TotalAmount { get; set; }
         public string Status { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
