c8dee50213825ed3d6a2925082d749b44b883437
Huynh Nguyen Thai Duong <115139619+hntduong12345@users.noreply.github.com>
2025-08-10 21:56:46 +0700

    fix: add missing handle reduce available create post quota in create post service
    - Add the reduce remain value in the subscription benefit when post is created.
    - Add email field in update account profile api

---
+++ FoodVietAPI.Data/Enums/SubscriptionContract/SubscriptionContractStatusEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanFoodVietAPI.Data.Enums.SubscriptionContract
+{
+    public enum SubscriptionContractStatusEnum
+    {
+        PENDING,
+        ACTIVE,
+        EXPRIED
+    }
+}
