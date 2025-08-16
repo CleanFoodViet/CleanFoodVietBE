65adf0602c43220bc974b3346175572bc05ae05f
Huynh Nguyen Thai Duong <115139619+hntduong12345@users.noreply.github.com>
2025-08-16 16:26:45 +0700

    feat: add  input constraint for service package create api

--- FoodVietAPI.Application/DTOs/ServicePackageDTOs/CreateServicePackageDTO.cs
+++ FoodVietAPI.Application/DTOs/ServicePackageDTOs/CreateServicePackageDTO.cs
@@ -1,10 +1,15 @@
-﻿namespace CleanFoodVietAPI.Application.DTOs.ServicePackageDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanFoodVietAPI.Application.DTOs.ServicePackageDTOs
 {
     public class CreateServicePackageDTO
     {
         public string PackageName { get; set; } = null!;
         public string? Description { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be 1 or greater.")]
         public decimal Price { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Duration must be 1 or greater.")]
         public int Duration { get; set; }
 
         // List of feature IDs that the admin wants to include.
